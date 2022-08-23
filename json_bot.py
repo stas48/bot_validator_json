64fc66924815ecba2d80c33c82984d3bcab05d81
Stanislav Makarov <72444479+stas48@users.noreply.github.com>
2022-08-23 13:32:39 +0300

    Update json_bot.py

--- json_bot.py
+++ json_bot.py
@@ -5,7 +5,7 @@
 from telebot import TeleBot, types
 import json
 
-bot = TeleBot(token='Вставь свой токен', parse_mode='html') # создание бота
+bot = TeleBot(token='5743464654:AAFeOWbgYd70zaar9OYEMUxg-cT8VedvxLU', parse_mode='html') # создание бота
 
 
 # обработчик команды '/start'
